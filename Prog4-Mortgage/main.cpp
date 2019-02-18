631a154d86d7727425be8dcf0de207e6962cb982
Preston <prestonharrod@gmail.com>
2019-02-18 13:52:34 -0600

    Defined getData fuction

--- Prog4-Mortgage/main.cpp
+++ Prog4-Mortgage/main.cpp
@@ -1,3 +1,33 @@
-
+#include <iostream>
+#include <string>
+#include "Mortgage.h"
+using namespace std;
 // main goes here. Code that *uses* the Mortgage class
 // goes in this file
+
+void getData(Mortgage &);
+void displayData(const Mortgage)
+int main()
+{
+	Mortgage userData;
+
+	getData(userData);
+}
+
+void getData()
+{
+	int terms = 0;
+	double iRate = 0.0;
+	double amount = 0.0;
+
+	cout << "Please enter your loan amount: " << endl;
+	cin >> amount;
+
+	cout << "Please enter annual interest rate: " << endl;
+	cin >> iRate;
+
+	cout << "Please enter the amount of terms in years: " << endl;
+	cin >> terms;
+
+}
+
