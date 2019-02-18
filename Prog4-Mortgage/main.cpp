c1a0305283e8692b0d981414c43efd89687f4f49
Preston <prestonharrod@gmail.com>
2019-02-18 13:55:51 -0600

    Included validation for entered data

--- Prog4-Mortgage/main.cpp
+++ Prog4-Mortgage/main.cpp
@@ -14,7 +14,7 @@ int main()
 	getData(userData);
 }
 
-void getData()
+void getData(Mortgage userData)
 {
 	int terms = 0;
 	double iRate = 0.0;
@@ -23,11 +23,29 @@ void getData()
 	cout << "Please enter your loan amount: " << endl;
 	cin >> amount;
 
+	while (userData.setAmount(amount) == false)
+	{
+		cout << "Please enter your loan amount: ";
+		cin >> iRate;
+	}
+
 	cout << "Please enter annual interest rate: " << endl;
 	cin >> iRate;
+	while (userData.setRate(iRate) == false)
+	{
+		cout << "Please enter annual interest rate: ";
+		cin >> iRate;
+	}
 
 	cout << "Please enter the amount of terms in years: " << endl;
 	cin >> terms;
+	while (userData.setTerm(terms) == false)
+	{
+		cout << "Please enter the number of terms in years: ";
+		cin >> terms;
+	}
 
 }
+
+void displayData
 
